6622a28b01828ac7ed0c3a23dafa6eabd542d956
Jaybee-v <j-b.vrain@outlook.fr>
2022-07-28 02:18:00 +0200

    changement des font

--- src/app/components/navbar-menu/navbar-menu.component.ts
+++ src/app/components/navbar-menu/navbar-menu.component.ts
@@ -1,12 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'navbar-menu',
   templateUrl: './navbar-menu.component.html',
   styleUrls: ['./navbar-menu.component.scss'],
 })
-export class NavbarMenuComponent implements OnInit {
+export class NavbarMenuComponent {
+  top: any;
+  left: any;
+  expand = false;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  @HostListener('document:click', ['$event'])
+  onClick($event: any) {
+    this.expand = true;
+    setTimeout(() => {
+      this.expand = false;
+    }, 500);
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  onMousemove($event: any) {
+    this.top = $event.pageY - 10 + 'px';
+    this.left = $event.pageX - 10 + 'px';
+  }
 }
